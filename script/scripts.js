9802fba1c75b8caf59536e4a3324abd464dce2a7
promise <promiseniyi@gmail.com>
2025-07-27 22:13:12 +0100

    updated

--- script/scripts.js
+++ script/scripts.js
@@ -1,11 +1,14 @@
 const menuToggle = document.getElementById('menuToggle');
 const menuPopup = document.getElementById('menuPopup');
 const overlay = document.getElementById('overlay');
+const closePopup = document.getElementById("closePopup");
 
 function showMenu() {
   menuPopup.classList.add('active');
   overlay.style.display = 'block';
-}
+}   
+
+
 
 function hideMenu() {
   menuPopup.classList.remove('active');
@@ -18,16 +21,26 @@ function hideMenu() {
   }, 100);
 }
 
+
+
+
 menuToggle.addEventListener('click', () => {
   if (menuPopup.classList.contains('active')) {
     hideMenu();
+    menuToggle.classList.add("hide");
   } else {
     menuPopup.style.display = 'block';
     // Force reflow before adding class to trigger animation
+    
     void menuPopup.offsetWidth;
     showMenu();
   }
 });
+
+closePopup.addEventListener("click", () => {
+    hideMenu();
+    menuToggle.classList.remove("hide");
+  });
 
 window.addEventListener('resize', () => {
   if (window.innerWidth > 900) {
